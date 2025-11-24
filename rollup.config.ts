3b446e447aa755d3ec996fb3850f334c78836bb9
Dan Martens <dan@friendsoftheweb.com>
2025-11-24 13:18:27 -0500

    Fix CJS build

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,17 +1,31 @@
 import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
 
-export default defineConfig({
-  input: ['src/index.ts'],
-  output: [
-    {
-      dir: 'dist/esm',
-      format: 'esm',
-      preserveModules: true,
-      exports: 'auto',
-      sourcemap: true,
-    },
-  ],
-  plugins: [typescript({ outDir: './dist/esm' })],
-  external: ['react', 'react/jsx-runtime', 'rxjs'],
-});
+export default [
+  defineConfig({
+    input: ['src/index.ts'],
+    output: [
+      {
+        dir: 'dist/cjs',
+        format: 'cjs',
+        preserveModules: true,
+        exports: 'auto',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript({ outDir: './dist/cjs' })],
+  }),
+  defineConfig({
+    input: ['src/index.ts'],
+    output: [
+      {
+        dir: 'dist/esm',
+        format: 'esm',
+        preserveModules: true,
+        exports: 'auto',
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript({ outDir: './dist/esm' })],
+  }),
+];
